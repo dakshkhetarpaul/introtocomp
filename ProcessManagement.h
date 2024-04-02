6ec5675eb2cc92ea724b558fed95c0bf34160a7a
Ali Eke <95650476+Alikerempro@users.noreply.github.com>
2024-04-02 14:12:04 -0400

    Update ProcessManagement.h

--- ProcessManagement.h
+++ ProcessManagement.h
@@ -1,10 +1,19 @@
-#ifndef PROCESSMANAGEMENT_H
-#define PROCESSMANAGEMENT_H
+#ifndef PROCESS_MANAGEMENT_H
+#define PROCESS_MANAGEMENT_H
 
-#include "IPC.h" // Include for communication functions
+#include "IPC.h"
+
+// Structure to store results from a child process
+typedef struct childResult {
+  int localMax;
+  double localAvg;
+  int hiddenKeys;
+  int *hiddenKeyPositions; // Array to store hidden key positions (assuming unique)
+  int numHiddenKeys;   // Number of hidden keys found (optional, for efficiency)
+} childResult_t;
 
 // Function to create a process tree (BFS or DFS) and distribute work
 void createProcessTree(int *data, int size, int pn, int startIndex, int depth, int isBFS,
-                       int *globalMax, double *globalAvg, int *totalHiddenKeys);
+                        int *globalMax, double *globalAvg, int *totalHiddenKeys);
 
-#endif // PROCESSMANAGEMENT_H
+#endif
