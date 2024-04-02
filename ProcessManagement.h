a0df8cab5fa3ad640e90058cc3ab028134548d11
Ali Eke <95650476+Alikerempro@users.noreply.github.com>
2024-04-02 11:41:02 -0400

    Update ProcessManagement.h

--- ProcessManagement.h
+++ ProcessManagement.h
@@ -1,6 +1,10 @@
 #ifndef PROCESSMANAGEMENT_H
 #define PROCESSMANAGEMENT_H
 
-void createProcesses(int depth);
+#include "IPC.h" // Include for communication functions
+
+// Function to create a process tree (BFS or DFS) and distribute work
+void createProcessTree(int *data, int size, int pn, int startIndex, int depth, int isBFS,
+                       int *globalMax, double *globalAvg, int *totalHiddenKeys);
 
 #endif // PROCESSMANAGEMENT_H
