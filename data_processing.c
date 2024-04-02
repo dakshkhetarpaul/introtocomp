824014352a669258f2380a7c8ec26d3decbef9e9
Ali Eke <95650476+Alikerempro@users.noreply.github.com>
2024-04-02 14:03:06 -0400

    Update data_processing.c

--- data_processing.c
+++ data_processing.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include "data_processing.h"
+
+int getDataSize(const char *filename) {
+  FILE *file = fopen(filename, "r");
+  if (file == NULL) {
+    return -1; // Error opening file
+  }
+
+  int count = 0;
+  while (fscanf(file, "%*d") != EOF) {
+    count++;
+  }
+
+  fclose(file);
+  return count;
+}
+
+int readFileIntoArray(const char *filename, int *data, int size) {
+  FILE *file = fopen(filename, "r");
+  if (file == NULL) {
+    return -1; // Error opening file
+  }
+
+  int numRead = 0;
+  for (int i = 0; i < size && fscanf(file, "%d", &data[i]) != EOF; ++i) {
+    numRead++;
+  }
+
+  fclose(file);
+  return numRead; // May be less than size if EOF is reached before reading all elements
+}
 
