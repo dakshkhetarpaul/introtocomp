bfa8c5f341ba57aa633ea9735d07a4bd4f951537
Ali Eke <95650476+Alikerempro@users.noreply.github.com>
2024-04-02 14:03:30 -0400

    Update data_processing.h

--- data_processing.h
+++ data_processing.h
@@ -1,1 +1,11 @@
+#ifndef DATA_PROCESSING_H
+#define DATA_PROCESSING_H
 
+// Function to get the size of the data file
+int getDataSize(const char *filename);
+
+// Function to read data from the file into an array
+int readFileIntoArray(const char *filename, int *data, int size);
+
+#endif
+ 
