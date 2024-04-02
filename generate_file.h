6189148def61b12da2d8bf86a7d7bc2602e474b6
Ali Eke <95650476+Alikerempro@users.noreply.github.com>
2024-04-02 14:03:57 -0400

    Update generate_file.h

--- generate_file.h
+++ generate_file.h
@@ -1,11 +1,7 @@
-#ifndef GENERATEFILE_H
-#define GENERATEFILE_H
+#ifndef GENERATE_FILE_H
+#define GENERATE_FILE_H
 
-// Function to generate the file with positive integers and hidden keys.
+// Function to generate the file with positive integers and hidden keys
 void generateFile(int L, int H);
 
-// Function to write results to a file.
-void writeResultsToFile(const char *filename, const char *results);
-
-#endif // GENERATEFILE_H
-
+#endif
