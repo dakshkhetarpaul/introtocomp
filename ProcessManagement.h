ac61841506761a5e0141d06556f5831b318e46e0
Omies-Ha <108757434+Omies-Ha@users.noreply.github.com>
2024-04-03 15:11:32 -0400

    Update ProcessManagement.h

--- ProcessManagement.h
+++ ProcessManagement.h
@@ -3,15 +3,14 @@
 
 // Structure to store results from a child process
 typedef struct childResult {
-  int localMax;
-  double localAvg;
-  int hiddenKeys;
-  int *hiddenKeyPositions; // Array to store hidden key positions (assuming unique)
-  int numHiddenKeys;// Number of hidden keys found (optional, for efficiency)
+    int localMax;
+    double localAvg;
+    int hiddenKeys;
+    int *hiddenKeyPositions; // Array to store hidden key positions (assuming unique)
+    int numHiddenKeys;       // Number of hidden keys found (optional, for efficiency)
 } childResult_t;
 
-// Function to create a process tree (BFS or DFS) and distribute work
 void createProcessTree(int *data, int size, int pn, int startIndex, int depth, int isBFS,
                         int *globalMax, double *globalAvg, int *totalHiddenKeys);
 
-#endif
+#endif // PROCESS_MANAGEMENT_H
